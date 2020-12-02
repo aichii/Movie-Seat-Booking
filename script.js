50ba0d72840bad28442a742948fa66512ed9d7a3
Aileen <aileenarcia073@gmail.com>
2020-12-01 21:12:07 -0500

    add local storage and populating in UI

--- script.js
+++ script.js
@@ -4,11 +4,28 @@ const count = document.getElementById('count')
 const total = document.getElementById('total')
 const movieSelect = document.getElementById('movie'); //gives the actual element
 
+populateUI();
+
 let ticketPrice = +movieSelect.value; //gives the value of selected movie 
+
+//save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
 
 //update total and count
 function updateSelectedCount(){
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+//copy selected seats into array
+//map through that array
+//return new array of indexes
+
+ const seatsIndex = [...selectedSeats].map(seat =>
+    [...seats].indexOf(seat)); 
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
   const selectedSeatsCount = selectedSeats.length;
 
@@ -16,11 +33,32 @@ function updateSelectedCount(){
   total.innerText = selectedSeatsCount * ticketPrice; //innerText is adjusted by the number calculated
 }
 
+//get data from local storage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats')
+  );
+
+  if (selectedSeats !==null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if (selectedMovieIndex !==null) {
+    movieSelect.selectedIndex = selectedMovieIndex 
+  }
+};
+
 //Movie select event
 movieSelect.addEventListener('change', (e) => {
   ticketPrice = +e.target.value
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
-})
+});
 
 //Seat select event
 container.addEventListener('click', (e) => {
@@ -30,4 +68,7 @@ container.addEventListener('click', (e) => {
 
    updateSelectedCount();
  } 
-})
+});
+
+//initial count and total set
+updateSelectedCount();
